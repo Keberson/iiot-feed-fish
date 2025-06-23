a2c988ccf401cb89a85908845975a28cc7d761e1
Keberson <creedkiller.62@gmail.com>
2025-06-23 19:26:15 +0300

    Правка entrypoint для бэка

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -3,9 +3,11 @@
 set -e
 
 echo "➤ Принудительное создание миграций..."
-python manage.py makemigrations --noinput
+python manage.py makemigrations
+python manage.py makemigrations fish_app
 
 echo "➤ Применение миграций..."
-python manage.py migrate --noinput
+python manage.py migrate
+python manage.py migrate fish_app
 
-exec gunicorn --bind ${BACK_HOST:-0.0.0.0}:${BACK_PORT:-8000} core.wsgi:application
+exec gunicorn --bind localhost:${BACK_PORT:-8000} core.wsgi:application
